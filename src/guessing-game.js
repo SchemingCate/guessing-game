e12e8ae91957dc59e9020200dcae922a2a76dc72
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-02-25 01:28:24 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
     constructor() {}
+    min = 0;
+    max = 0;
+    guessedNum = 0;
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessedNum = Math.ceil((this.min + this.max) / 2);
+        return this.guessedNum;
     }
 
     lower() {
-
+        this.max = this.guessedNum;
     }
 
     greater() {
-
+        this.min = this.guessedNum;
     }
 }
 
